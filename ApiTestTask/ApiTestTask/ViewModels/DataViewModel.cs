9400f8ee3bf76d1081c0dcae31bfa6392b7641d8
daniel-isme <daniil.melnikov.1999@mail.ru>
2020-08-24 15:28:48 +0700

    Color for header

--- ApiTestTask/ApiTestTask/ViewModels/DataViewModel.cs
+++ ApiTestTask/ApiTestTask/ViewModels/DataViewModel.cs
@@ -81,7 +81,6 @@ namespace ApiTestTask.ViewModels
             if (allDataJson != null)
             {
                 var allDataObject = JsonConvert.DeserializeObject<JsonObjectModel>(allDataJson);
-                var dataSeriesHeader = allDataObject.Data.DataSeriesHeader;
                 DataSeries = allDataObject.Data.DataSeries;
                 DataSeriesHeader = allDataObject.Data.DataSeriesHeader;
                 foreach (var dataRow in DataSeries)
@@ -90,6 +89,10 @@ namespace ApiTestTask.ViewModels
                     else if (dataRow.Diff > 0) dataRow.Color = Color.Green;
                     else dataRow.Color = ColorConverters.FromHex("#b2b5bc");
                 }
+
+                if (DataSeriesHeader.Diff < 0) DataSeriesHeader.Color = ColorConverters.FromHex("#f98883");
+                else if (DataSeriesHeader.Diff > 0) DataSeriesHeader.Color = Color.Green;
+                else DataSeriesHeader.Color = ColorConverters.FromHex("#b2b5bc");
             }
             else
             {
@@ -104,9 +107,15 @@ namespace ApiTestTask.ViewModels
                 return null;
             }            
 
+            // ошибка 404
             //string url = "https://uc51dcd2fde5d5d87d793279c0af.dl.dropboxusercontent.com/cd/0/get/A93zuT7EnXcCC6MvhEy0F1SCUnt84zmse6jkBnm-Ex3UJoh0dVkiiT5NY9wj4DBtyXCxWFoda4ScMtNGqbgGI48hesP2b0u3G-CcRwmRlguNfdIeNczjZNP6UEqx-S_UvkM/file";
+
+            // здесь все хорошо
             string url = "https://uc2e830cb90ae8f8a417e8b0a5f3.dl.dropboxusercontent.com/cd/0/get/A-ARvkcAScQYq8t3v_n-QXk5YipVKH053C-y8xtwhIaJrf_GB6itLfmXDeRjw0SlLYpK7UJW7ylzIBb53J6pEDrb1V_vUdseKu8TGeUf9jr_RZCp5wexAZxlnzVndttAYec/file#";
+            
+            // здесь нет данных
             //string url = "https://uce04a6c7e9e2a30135b2bb87acc.dl.dropboxusercontent.com/cd/0/get/A-BZ1tVh9mlMiHy4s_6ZDEG2WNHKwA19ABGVVV-NaPICPuaEUPHJk5HBK6mRsMZMR7CdegrzH9IUasFWZg6Cfr1OP0Q_bikPZ9yJWq5ckTl6FuxHVcoKn43E13vDXKqnBRg/file?dl=1";
+            
             string json = string.Empty;
                         
             using (WebClient wc = new WebClient())
